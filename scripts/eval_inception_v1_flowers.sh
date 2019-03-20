d27fea638ec666821b0613a6350956f55d289f93
dupei <dupei.dp@antfin.com>
2019-03-20 15:56:33 +0800

    update evaluation scripts

--- scripts/eval_inception_v1_flowers.sh
+++ scripts/eval_inception_v1_flowers.sh
@@ -1,4 +1,4 @@
-CHECKPOINT_FILE=./model_inception_v1_flowers
+CHECKPOINT_FILE=/data/models/model_inception_v1_flowers
 DATASET_DIR=/data/image-data/flowers_slim
 python eval_image_classifier.py \
     --alsologtostderr \
